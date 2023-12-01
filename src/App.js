2351f786ef4c560e788057368f75c508248be789
mac <mac@macs-MacBook-Pro.local>
2023-12-01 13:01:14 +0530

    created backend using node js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // Update the import statement
 import Main from './Component/Main';
-import Signup from './Component/Signup';
+// import Signup from './Component/Signup';
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Routes>  {/* Replace Switch with Routes */}
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/" element={<Main />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <Main/>
+    </div>
+    // <Router>
+    //   <div>
+    //     <Routes>  {/* Replace Switch with Routes */}
+    //       <Route path="/signup" element={<Signup />} />
+    //       <Route path="/" element={<Main />} />
+    //     </Routes>
+    //   </div>
+    // </Router>
   );
 };
 
